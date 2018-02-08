de0d30e197b0e6d67eed77324d8bab1cfaff1b4e
Jocelyn1912 <toty_2410@hotmail.com>
2018-02-08 09:54:35 -0300

    Corrección estilos google

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  $('#name,#password').keydown(function() {
+    var name = $('#name').val();
+    var pass = $('#password').val();
 
-  $("#name,#password").keydown(function(){
-    var name = $("#name").val();
-    var pass = $("#password").val();
-
-    for (var i = 0; i < 1; i++){
-      if (name.length != "" && pass.length >= 6){
-        $("#login").removeAttr("disabled")
-        $("#login").removeAttr("class");
-        $("#login").addClass("active");
-      }else{
-        $("#login").attr("disabled","disabled")
-        $("#login").removeAttr("class");
-        $("#login").addClass("inactive");
+    for (var i = 0; i < 1; i++) {
+      if (name.length !== '' && pass.length >= 6) {
+        $('#login').removeAttr('disabled');
+        $('#login').removeAttr('class');
+        $('#login').addClass('active');
+      } else {
+        $('#login').attr('disabled","disabled');
+        $('#login').removeAttr('class');
+        $('#login').addClass('inactive');
       };
     };
   });
-
-});//ready
+});// ready
--- assets/js/collage.js
+++ assets/js/collage.js
@@ -1,13 +1,16 @@
+/*Funciones con las que podemos mover las fotos*/
 function drag(evento) {
-  console.log(evento.target.src);
+  // Tomamos las fotos usando su id
   evento.dataTransfer.setData('text', evento.target.id);
 }
 
-function permitirDrop(evento){
+function permitirDrop(evento) {
   evento.preventDefault();
 }
+
 function drop(evento) {
   evento.preventDefault();
+  // Recuperando los datos y dejandolos en el contenedor
   var foto = evento.dataTransfer.getData('text');
   evento.target.appendChild(document.getElementById(foto));
 }
